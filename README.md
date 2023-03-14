5e75a6feacf13aeafe2cf22025ddf2cad0c0fbb2
ale <malejlopez@hotmail.com>
2023-03-14 02:43:28 -0400

    Started new website with react and tailwind to substitute astro project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # areweleetcodeyet
 leetcode crawler and website to check compatibility with leetcode questions in rust 
+
+## Frontend Website React+Vite+SWC
+`yarn create vite`
+- options:
+- react, typescript with SWC
+
+
+### Tailwind
+- padding spacing inside the element
+- margin spacin outside the element
---
+++ react-vite-website/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className='flex justify-center flex-col items-center'>
+      <h1 className='text-3xl text-green-600'>Are We Leetcode Yet?</h1>
+      <p className='text-3xl '>Keeping track of leetcode questions</p>
+    </div>
+  );
+};
+
+export default App;
